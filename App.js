987fa58f72faa061003869fc04104cbf5cfe7bc0
Mauricio Xavier <mcxavier2@gmail.com>
2018-06-06 15:47:49 -0300

    inicio
    inicio...

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Mauricio é muito foda!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
